a7c78a5c6fca63dc63dbee25f20c1383ea902aa2
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-10-20 11:39:59 +0000

    Add functionality for User to Curse their Victim

---
+++ assets/js/script.js
@@ -0,0 +1,98 @@
+const FIRST_PHRASE = document.getElementById("firstResult");
+const SECOND_PHRASE = document.getElementById("secondResult");
+const THIRD_PHRASE = document.getElementById("thirdResult");
+
+// **** ELEMENT DISABLE CODE ****
+const NAME_ELEMENT = document.getElementById("name");
+
+let phraseListOne = [
+  "will",
+  "shall always",
+  "is cursed to",
+  "will forever",
+  "is destined to",
+  "is fated to",
+];
+let phraseListTwo = [
+  "be covered in",
+  "be drenched in",
+  "suffer under",
+];
+let phraseListThree = [
+  "a mountain of Beetlejuice's poop!",
+  "buckets of Beetlejuice spittle!",
+  "a lake of boiling Beetle'JUICE! (whatever that is, use your imagination)",
+  "rotten corpses for a million and one years! ...that is such a long time",
+];
+
+let phraseListIndexMap = [phraseListOne, phraseListTwo, phraseListThree];
+let phraseElementIndexMap = [FIRST_PHRASE, SECOND_PHRASE, THIRD_PHRASE];
+let generateCurse = function (e) {
+  if (!checkName()) {
+    return;
+  }
+
+  let buttonIndex = parseInt(e.target.dataset.index) - 1;
+  generateText(buttonIndex);
+
+  // **** ELEMENT DISABLE CODE ****
+  CURSE_BUTTONS.forEach((button) => (button.classList.add("disabled")));
+
+  if (buttonIndex != 2) {
+    CURSE_BUTTONS[buttonIndex + 1].classList.remove("disabled");
+    } else {
+      buttonFinal.classList.remove("disabled");
+    }
+};
+
+// **** GENERATE RANDOM PHRASE FROM ARRAYS CODE ****
+const CURSE_BUTTONS = Array.from(document.getElementsByClassName("curse-button"));
+CURSE_BUTTONS.forEach((button) => button.addEventListener("click", generateCurse)
+);
+
+let generateText = function (index) {
+  let phraseList = phraseListIndexMap[index];
+  let phrase = phraseList[Math.floor(Math.random() * phraseList.length)];
+
+  let phraseElement = phraseElementIndexMap[index];
+  phraseElement.innerHTML = phrase;
+};
+
+// **** ELEMENT DISABLE CODE ****
+NAME_ELEMENT.addEventListener("focusout", () =>
+  CURSE_BUTTONS[0].classList.remove("disabled")
+);
+
+// **** GENERATE FULL HEX CODE ****
+const buttonFinal = document.getElementById("finalButton");
+buttonFinal.addEventListener("click", (e) => {
+  
+  if (checkName()) {
+    genHex();
+  }
+
+});
+
+// **** ENSURE USER INPUTS TEXT TO START HEX CODE ****
+function checkName() {
+  if (document.getElementById("name").value !== "") {
+    
+    return true;
+  } 
+
+  alert("please enter your victim's name to proceed");
+  return false;
+}
+
+// **** BACK TO TOP AND RELOAD PAGE CODE ****
+const refreshPage = document.getElementById("refresh");
+refreshPage.addEventListener("click", (e) => {
+  document.location.reload();
+});
+
+function genHex() {
+  let finalPhrase = document.getElementById("finalResult");
+  let name = document.getElementById("name").value;
+
+  finalPhrase.innerHTML = `${name} ${FIRST_PHRASE.innerHTML} ${SECOND_PHRASE.innerHTML} ${THIRD_PHRASE.innerHTML}`;
+}
