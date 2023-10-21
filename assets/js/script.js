022f42aad29de75dc586a5ce48b03693e7f9d927
L0rdGabbersReviewer <148454556+L0rdGabbersReviewer@users.noreply.github.com>
2023-10-21 13:34:55 +0100

    Fixes empty input issue and properly generates sentences. (#44)
    Co-authored-by: L0rdGabbers <gabrielmarriage@me.com>
    Co-authored-by: L0rdGabbers <119704111+L0rdGabbers@users.noreply.github.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,23 +6,70 @@ const THIRD_PHRASE = document.getElementById("thirdResult");
 const NAME_ELEMENT = document.getElementById("name");
 
 let phraseListOne = [
-  "will",
-  "shall always",
-  "is cursed to",
-  "will forever",
-  "is destined to",
-  "is fated to",
+  "Shall forever",
+  "Is cursed to",
+  "Will always",
+  "Is fated to",
+  "Shall eternally",
+  "Is destined to",
+  "Will persistently",
+  "Shall be bound to",
+  "Is condemned to",
+  "Will forever",
 ];
 let phraseListTwo = [
-  "be covered in",
-  "be drenched in",
-  "suffer under",
+  "Be covered in",
+  "Be drenched in",
+  "Suffer under",
+  "Endure",
+  "Wade through",
+  "Experience",
+  "Face",
+  "Confront",
+  "Bear",
+  "Endure the torment of",
 ];
 let phraseListThree = [
+  // "a mountain of Beetlejuice's poop!",
+  // "buckets of Beetlejuice spittle!",
+  // "a lake of boiling Beetle'JUICE! (whatever that is, use your imagination)",
+  // "rotten corpses for a million and one years! ...that is such a long time",
+  // "your shadow takes on a mischievous life of its own.",
+  // "your favorite costume wig becomes possessed and turns into a sentient Medusa headpiece.",
+  // "Jack-o'-lanterns spontaneously burst into spooky ghost flames.",
+  // "your bathroom mirror always reveals your inner ghostly self.",
+  // "Halloween decorations come to life, staging their own monster parade.",
+  // "house only accessible through secret doorways to unexpected places.",
+  // "candy bowl filled with candy corn that multiplies when you look away.",
+  // "footsteps in the dark always sound like eerie tap dancing.",
+  // "house is haunted by invisible sandworms.",
+  // "candy wrappers scream when you try to open them.",
+  // "refrigerator is forever stocked with expired ghostly leftovers.",
+  // "alarm clock plays 'Day-O' song every morning.",
+  // "house filled with mirrors that only reflect your worst fears.",
+  // "candy bowl dispenses live snakes instead of treats.",
+  // "yard decorations turn into a zombie dance party at midnight.",
+  // "Halloween party guests become invisible and play pranks on each other.",
+  // "beetlejuice movie plays on loop for a week.",
+  // "kitchen utensils take on a life of their own, performing a spooky symphony.",
+  // "front door becomes a portal to the Netherworld for mischievous spirits.",
+  // "candy apples turn into mini crystal balls that reveal your future.",
+  // "Halloween masks reveal your true face to the world.",
+
   "a mountain of Beetlejuice's poop!",
   "buckets of Beetlejuice spittle!",
   "a lake of boiling Beetle'JUICE! (whatever that is, use your imagination)",
   "rotten corpses for a million and one years! ...that is such a long time",
+  "A mountain of candy corn",
+  "Buckets of ghostly treats",
+  "A lake of witches' brew",
+  "Rotten pumpkins for a thousand Halloweens",
+  "Eerie whispers in the moonlight",
+  "Spectral footsteps in the dark",
+  "An endless night of spooktacular puns",
+  "Cauldrons bubbling with enchanted brews",
+  "Ghosts and goblins at the doorstep",
+  "Skeletons dancing to the monster mash",
 ];
 
 let phraseListIndexMap = [phraseListOne, phraseListTwo, phraseListThree];
@@ -40,10 +87,10 @@ let generateCurse = function (e) {
 
   if (buttonIndex != 2) {
     CURSE_BUTTONS[buttonIndex + 1].classList.remove("disabled");
-} else {
-    let finalButton = document.getElementById("finalButton");
-        finalButton.classList.remove("disabled");
-}
+    } else {
+      buttonFinal.classList.remove("disabled");
+    }
+};
 
 // **** GENERATE RANDOM PHRASE FROM ARRAYS CODE ****
 const CURSE_BUTTONS = Array.from(document.getElementsByClassName("curse-button"));
@@ -64,8 +111,8 @@ NAME_ELEMENT.addEventListener("focusout", () =>
 );
 
 // **** GENERATE FULL HEX CODE ****
-const finalButton = document.getElementById("finalButton");
-finalButton.addEventListener("click", (e) => {
+const buttonFinal = document.getElementById("finalButton");
+buttonFinal.addEventListener("click", (e) => {
   
   if (checkName()) {
     genHex();
@@ -76,12 +123,11 @@ finalButton.addEventListener("click", (e) => {
 // **** ENSURE USER INPUTS TEXT TO START HEX CODE ****
 function checkName() {
   if (document.getElementById("name").value !== "") {
-    
+    document.getElementById("name").placeholder = ""
     return true;
-  } 
-  else {
-  alert("please enter your victim's name to proceed");
-  return false;
+  } else {
+    document.getElementById("name").placeholder = "I require a name!!!"
+    return false;
 }}
 
 // **** BACK TO TOP AND RELOAD PAGE CODE ****
@@ -93,8 +139,8 @@ refreshPage.addEventListener("click", (e) => {
 function genHex() {
   let finalPhrase = document.getElementById("finalResult");
   let name = document.getElementById("name").value;
-
-  finalPhrase.innerHTML = `${name} ${FIRST_PHRASE.innerHTML} ${SECOND_PHRASE.innerHTML} ${THIRD_PHRASE.innerHTML}`;
+  phrase = `${FIRST_PHRASE.innerHTML} ${SECOND_PHRASE.innerHTML} ${THIRD_PHRASE.innerHTML}`;
+  finalPhrase.innerHTML = name + " " + phrase.toLowerCase() + "!";
 }
 
 // Function for playing scary sounds
@@ -108,4 +154,12 @@ document.addEventListener('click', function (event) {
       playSound();
     }
   });
+
+//** VIDEO OPEN IN POP-UP */
+function openVideo(videoId, videoLabel) {
+    var iframe = document.getElementById("video");
+    iframe.src = "https://www.youtube.com/embed/" + videoId;
+    document.getElementById("popup").style.display = "block";
 }
+    // Set the label for the video
+    document.getElementById("video-label").textContent = videoLabel;
